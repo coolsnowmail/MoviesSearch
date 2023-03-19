e0608ddb811a35b615bba34ec3f02d6549cf628a
Анастасия <cool.sbow@mail.ru>
2023-03-19 22:20:31 +0300

    task 20.2.2

--- app/src/main/java/com/example/moviessearch/MainActivity.kt
+++ app/src/main/java/com/example/moviessearch/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val text = findViewById<TextView>(R.id.textView)
-        text.setText("sdsadsadasdsadasasd")
+//        val text = findViewById<TextView>(R.id.textView)
+//        text.setText("sdsadsadasdsadasasd")
     }
 }
