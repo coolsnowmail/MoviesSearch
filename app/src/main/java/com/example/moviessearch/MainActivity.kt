15375738cdd71ad515db2d1cdba2cc21670e2980
Анастасия <cool.sbow@mail.ru>
2023-03-21 23:12:20 +0300

    add viewBinding (syntetic was depricated)

--- app/src/main/java/com/example/moviessearch/MainActivity.kt
+++ app/src/main/java/com/example/moviessearch/MainActivity.kt
@@ -6,16 +6,19 @@ import android.view.Gravity
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import com.megamovies.moviessearch.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-//        val text = findViewById<TextView>(R.id.textView)
-//        text.setText("sdsadsadasdsadasasd")
-        button_toast.setOnClickListener
+//        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root) // we now set the contentview as the binding.root
+
+        binding.button.text = "Hallo and welcome"
+
     }
 
 //    fun onClickToast(view: View) {
