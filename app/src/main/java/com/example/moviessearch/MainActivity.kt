571fba4880fa73ce0d5b5984ef80342445c3881a
And <cool.snow@mail.ru>
2023-03-27 16:58:16 +0300

    task 20_8 passing task 6 (addind toast on buttons)

--- app/src/main/java/com/example/moviessearch/MainActivity.kt
+++ app/src/main/java/com/example/moviessearch/MainActivity.kt
@@ -3,6 +3,8 @@ package com.megamovies.moviessearch
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.Toast
+import android.widget.Toast.makeText
 import com.megamovies.moviessearch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +15,10 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.title = "MoviesSearcher"
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.button1?.setOnClickListener{makeText(this, binding.button1.text, Toast.LENGTH_SHORT).show()}
+        binding.button2?.setOnClickListener{makeText(this, binding.button2.text, Toast.LENGTH_SHORT).show()}
+        binding.button3?.setOnClickListener{makeText(this, binding.button3.text, Toast.LENGTH_SHORT).show()}
+        binding.button4?.setOnClickListener{makeText(this, binding.button4.text, Toast.LENGTH_SHORT).show()}
+        binding.button5?.setOnClickListener{makeText(this, binding.button5.text, Toast.LENGTH_SHORT).show()}
     }
 }
