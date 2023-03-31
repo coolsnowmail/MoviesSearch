398ed3f7206a749317973b31b984eba23edd0720
Анастасия <cool.sbow@mail.ru>
2023-04-01 00:25:10 +0300

    udemy viewbinding 3 lesson

---
+++ app/src/main/java/com/example/moviessearch/FirstFragment.kt
@@ -0,0 +1,27 @@
+package com.example.moviessearch
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.megamovies.moviessearch.databinding.FragmentBinding
+
+class FirstFragment : Fragment() {
+    private var _binding: FragmentBinding? = null
+    private val binding : FragmentBinding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+
+}
