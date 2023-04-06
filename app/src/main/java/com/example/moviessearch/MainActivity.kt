fccc42d37fc96f9ca74b848361139575d02a42c3
Анастасия <cool.sbow@mail.ru>
2023-04-06 22:43:03 +0300

    21.8 navigation adding

--- app/src/main/java/com/example/moviessearch/MainActivity.kt
+++ app/src/main/java/com/example/moviessearch/MainActivity.kt
@@ -15,14 +15,78 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.title = "MoviesSearcher"
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        buttonInit()
+        initNavigation()
+
+//        buttonInit()
     }
 
-    private fun buttonInit() {
-        binding.button1?.setOnClickListener{makeText(this, binding.button1.text, Toast.LENGTH_SHORT).show()}
-        binding.button2?.setOnClickListener{makeText(this, binding.button2.text, Toast.LENGTH_SHORT).show()}
-        binding.button3?.setOnClickListener{makeText(this, binding.button3.text, Toast.LENGTH_SHORT).show()}
-        binding.button4?.setOnClickListener{makeText(this, binding.button4.text, Toast.LENGTH_SHORT).show()}
-        binding.button5?.setOnClickListener{makeText(this, binding.button5.text, Toast.LENGTH_SHORT).show()}
+    private fun initNavigation() {
+        binding.topAppBar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.settings -> {
+                    Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+
+        binding.bottomNavigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.favorites -> {
+                    Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.watch_later -> {
+                    Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.selections -> {
+                    Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
     }
+
+//    private fun buttonInit() {
+//        binding.button1?.setOnClickListener {
+//            makeText(
+//                this,
+//                binding.button1.text,
+//                Toast.LENGTH_SHORT
+//            ).show()
+//        }
+//        binding.button2?.setOnClickListener {
+//            makeText(
+//                this,
+//                binding.button2.text,
+//                Toast.LENGTH_SHORT
+//            ).show()
+//        }
+//        binding.button3?.setOnClickListener {
+//            makeText(
+//                this,
+//                binding.button3.text,
+//                Toast.LENGTH_SHORT
+//            ).show()
+//        }
+//        binding.button4?.setOnClickListener {
+//            makeText(
+//                this,
+//                binding.button4.text,
+//                Toast.LENGTH_SHORT
+//            ).show()
+//        }
+//        binding.button5?.setOnClickListener {
+//            makeText(
+//                this,
+//                binding.button5.text,
+//                Toast.LENGTH_SHORT
+//            ).show()
+//        }
+//    }
 }
