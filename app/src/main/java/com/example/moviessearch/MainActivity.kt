70b42de94e574ef806a1346fb626ec079e36660c
San Andres <cool.snow@mail.ru>
2023-06-29 22:35:50 +0300

    24.2 Snackbar

--- app/src/main/java/com/example/moviessearch/MainActivity.kt
+++ app/src/main/java/com/example/moviessearch/MainActivity.kt
@@ -2,6 +2,9 @@ package com.megamovies.moviessearch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
 import com.megamovies.moviessearch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +14,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val snackbar = Snackbar.make(binding.mainLayout, "Snackbar", Snackbar.LENGTH_INDEFINITE)
+        snackbar.setAction("action") {
+            Toast.makeText(this, "Toast", Toast.LENGTH_SHORT).show()
+        }
+        snackbar.setActionTextColor(ContextCompat.getColor(this, androidx.appcompat.R.color.error_color_material_dark))
+        binding.buttonGetSnack.setOnClickListener {
+            snackbar.show()
+        }
     }
 }
