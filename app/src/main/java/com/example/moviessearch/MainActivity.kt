80769e48328598c374371462e3752c3d1587c577
San Andres <cool.snow@mail.ru>
2023-06-29 23:33:39 +0300

    24.1 Actionbar (play testing)

--- app/src/main/java/com/example/moviessearch/MainActivity.kt
+++ app/src/main/java/com/example/moviessearch/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.megamovies.moviessearch
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -10,17 +11,12 @@ import com.megamovies.moviessearch.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    @SuppressLint("RestrictedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val snackbar = Snackbar.make(binding.mainLayout, "Snackbar", Snackbar.LENGTH_INDEFINITE)
-        snackbar.setAction("action") {
-            Toast.makeText(this, "Toast", Toast.LENGTH_SHORT).show()
-        }
-        snackbar.setActionTextColor(ContextCompat.getColor(this, androidx.appcompat.R.color.error_color_material_dark))
-        binding.buttonGetSnack.setOnClickListener {
-            snackbar.show()
-        }
+
+
     }
 }
