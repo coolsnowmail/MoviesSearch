bb7b508f5175fdd4530f29defffde3f37e51efe6
San Andres <cool.snow@mail.ru>
2023-07-01 15:33:17 +0300

    24.1 Actionbar (copied from education SF websrc)

--- app/src/main/java/com/example/moviessearch/MainActivity.kt
+++ app/src/main/java/com/example/moviessearch/MainActivity.kt
@@ -11,12 +11,13 @@ import com.megamovies.moviessearch.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
     @SuppressLint("RestrictedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+//        val snackbar = Snackbar.make()
 
     }
 }
