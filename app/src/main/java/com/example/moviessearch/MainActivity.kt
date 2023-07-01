c5343b2c12bb91fd7db6ef3c8989f69a2d651d11
San Andres <cool.snow@mail.ru>
2023-07-01 17:59:25 +0300

    24.3

--- app/src/main/java/com/example/moviessearch/MainActivity.kt
+++ app/src/main/java/com/example/moviessearch/MainActivity.kt
@@ -3,8 +3,10 @@ package com.megamovies.moviessearch
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 import android.widget.Toast
 import androidx.core.content.ContextCompat
+import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.snackbar.Snackbar
 import com.megamovies.moviessearch.databinding.ActivityMainBinding
 
@@ -21,5 +23,17 @@ class MainActivity : AppCompatActivity() {
         binding.fab.setOnClickListener {
             snackbar.show()
         }
+
+
+        binding.appBar.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { appBarLayout, verticalOffset ->
+           if(verticalOffset == 0) {
+               binding.toolbarLayout.setExpandedTitleColor(ContextCompat.getColor(this, R.color.black))
+           }
+            if (verticalOffset == appBarLayout.scrollBarSize) {
+               binding.toolbarLayout.setExpandedTitleColor(ContextCompat.getColor(this, androidx.appcompat.R.color.abc_secondary_text_material_dark))
+
+            }
+            binding.toolbarLayout.title = verticalOffset.toString()
+        })
     }
 }
