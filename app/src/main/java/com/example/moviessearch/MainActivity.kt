65c02734b5125aed9fd000e98cf66870ee3c66f6
And <cool.snow@mail.ru>
2023-07-20 16:34:05 +0300

    27.4 search a try with toolbar

--- app/src/main/java/com/example/moviessearch/MainActivity.kt
+++ app/src/main/java/com/example/moviessearch/MainActivity.kt
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.InputFilter
 import android.text.Spanned
+import android.view.Menu
+import android.widget.SearchView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import com.example.moviessearch.DetailsFragment
 import com.example.moviessearch.FavoritesFragment
 import com.example.moviessearch.FilmListRecyclerAdapter
@@ -15,18 +18,63 @@ import com.megamovies.moviessearch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val list = mutableListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.title = "MoviesSearcher"
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        for (i in 0..9) {
+            list.add("item$i")
+        }
+
+
+
+
+
+
         initNavigation()
         supportFragmentManager.beginTransaction().add(R.id.fragment_placeholder, HomeFragment())
             .addToBackStack(
                 null
             ).commit()
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.search_menu, menu)
+        val menuItem = menu?.findItem(R.id.search)
+        val searchView = menuItem?.actionView as SearchView
+        searchView.setOnQueryTextListener(object :
+            SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                if (list.contains(p0)) {
+
+                    binding.topAppBarSearch.title = "In list"
+                    binding.topAppBarSearch.setTitleTextColor(
+                        ContextCompat.getColor(
+                            this@MainActivity,
+                            R.color.green
+                        )
+                    )
+                } else {
+                    binding.topAppBarSearch.title = "Not in list"
+                    binding.topAppBarSearch.setTitleTextColor(
+                        ContextCompat.getColor(
+                            this@MainActivity,
+                            R.color.red
+                        )
+                    )
+                }
+                return false
+            }
+        })
+        return super.onCreateOptionsMenu(menu)
     }
 
     var backPressed = 0L
