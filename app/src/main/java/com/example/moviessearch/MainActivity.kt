e2fbd0db41d9e17ded33413511ef5a92c3ff5c07
And <cool.snow@mail.ru>
2023-08-11 15:03:28 +0300

    clean app for 29.2

--- app/src/main/java/com/example/moviessearch/MainActivity.kt
+++ app/src/main/java/com/example/moviessearch/MainActivity.kt
@@ -17,97 +17,16 @@ import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private var isRevealed = false
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        supportActionBar?.title = "MoviesSearcher"
+        supportActionBar?.title = "Fling"
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.buttonsContainer.visibility = View.GONE
-        binding.fab.setOnClickListener {
-            if (!isRevealed) {
-                reveal()
-            } else {
-                hide()
-            }
-        }
+
+
     }
 
-//    private fun reveal() {
-//        val x: Int = binding.fab.x.roundToInt() + binding.fab.width / 2
-//        val y: Int = binding.fab.y.roundToInt() + binding.fab.height / 2
-//        val endRadius = hypot(
-//            binding.buttonsContainer.width.toDouble(),
-//            binding.buttonsContainer.height.toDouble()
-//        )
-//        val startRadus = 0
-//        val anim = ViewAnimationUtils.createCircularReveal(
-//            binding.buttonsContainer, x, y, startRadus.toFloat(),
-//            endRadius.toFloat()
-//        )
-//        anim.duration = 1000
-//        binding.buttonsContainer.visibility = View.VISIBLE
-//        anim.start()
-//        isRevealed = true
-//    }
-
-    private fun reveal() {
-        val x: Int = binding.fab.x.roundToInt() + binding.fab.width / 2
-        val y: Int = binding.fab.y.roundToInt() + binding.fab.height / 2
-
-        val startRadius = 0
-        binding.buttonsContainer
-        val endRadius = hypot(binding.buttonsContainer.width.toDouble(), binding.buttonsContainer.height.toDouble())
-        val anim = ViewAnimationUtils.createCircularReveal(binding.buttonsContainer, x, y, startRadius.toFloat(), endRadius.toFloat())
-        anim.duration = 500
-        binding.buttonsContainer.visibility = View.VISIBLE
-        anim.start()
-        isRevealed = true
-    }
-
-
-    private fun hide() {
-        val x: Int = binding.fab.x.roundToInt() + binding.fab.width / 2
-        val y: Int = binding.fab.y.roundToInt() + binding.fab.height / 2
-        val startRadius: Int = binding.mainContainer.width.coerceAtLeast(binding.buttonsContainer.height)
-        val endRadius = 0
-
-        val anim = ViewAnimationUtils.createCircularReveal(binding.buttonsContainer, x, y, startRadius.toFloat(), endRadius.toFloat())
-        anim.duration = 500
-
-        //Слушатель конца анимации, когда анимация закончится, мы скроем View
-        anim.addListener(object : AnimatorListenerAdapter() {
-            override fun onAnimationEnd(animator: Animator) {
-                binding.buttonsContainer.visibility = View.GONE
-            }
-        })
-        anim.start()
-        isRevealed = false
-    }
-//    private fun hideButContainer() {
-//        val x: Int = binding.fab.x.roundToInt() + binding.fab.width / 2
-//        val y: Int = binding.fab.y.roundToInt() + binding.fab.height / 2
-//        val startRadius: Int =
-//            binding.mainContainer.width.coerceAtLeast(binding.buttonsContainer.height)
-//
-//
-//        val endRadius = 0
-//
-//        val anim = ViewAnimationUtils.createCircularReveal(
-//            binding.fab, x, y, startRadius.toFloat(),
-//            endRadius.toFloat()
-//        )
-//        anim.addListener(object : AnimatorListenerAdapter() {
-//            override fun onAnimationEnd(animation: Animator) {
-//                binding.buttonsContainer.visibility = View.GONE
-//            }
-//        })
-//        anim.duration = 1000
-//        anim.start()
-//        isRevealed = false
-//    }
 }
 
 
