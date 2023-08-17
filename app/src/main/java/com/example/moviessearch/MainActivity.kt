e84fc9cf34894668a2e2ab87e3c03f40e0d8570e
And <cool.snow@mail.ru>
2023-08-17 16:03:48 +0300

    clean app for 29.3

--- app/src/main/java/com/example/moviessearch/MainActivity.kt
+++ app/src/main/java/com/example/moviessearch/MainActivity.kt
@@ -3,6 +3,7 @@ package com.megamovies.moviessearch
 
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.GestureDetector
@@ -11,8 +12,11 @@ import android.view.View
 import android.view.ViewAnimationUtils
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import androidx.dynamicanimation.animation.DynamicAnimation
 import androidx.dynamicanimation.animation.FlingAnimation
+import androidx.dynamicanimation.animation.SpringAnimation
+import androidx.dynamicanimation.animation.SpringForce
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.megamovies.moviessearch.databinding.ActivityMainBinding
 import kotlin.math.hypot
@@ -21,41 +25,58 @@ import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var diffX = 0f
+    private var diffY = 0f
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.title = "Fling"
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val flingX = FlingAnimation(binding.flingView, DynamicAnimation.X).apply {
-            friction = 1.1f
+
+        val springForce = SpringForce(0f).apply {
+            stiffness = SpringForce.STIFFNESS_MEDIUM
+            dampingRatio = SpringForce.DAMPING_RATIO_MEDIUM_BOUNCY
         }
-        val flingY = FlingAnimation(binding.flingView, DynamicAnimation.Y).apply {
-            friction = 1.1f
-        }
-        val gestureListener = object : GestureDetector.SimpleOnGestureListener() {
-            override fun onFling(
-                e1: MotionEvent,
-                e2: MotionEvent,
-                velocityX: Float,
-                velocityY: Float
-            ): Boolean {
-                flingX.setStartVelocity(velocityX)
-                flingY.setStartVelocity(velocityY)
+        val spring_view = binding.flingView
+        val springAnimationX =
+            SpringAnimation(spring_view, DynamicAnimation.TRANSLATION_X).setSpring(springForce)
+        val springAnimationY =
+            SpringAnimation(spring_view, DynamicAnimation.TRANSLATION_Y).setSpring(springForce)
 
-                flingX.start()
-                flingY.start()
-
-                return true
-            }
-        }
-
-        val gestureDetector = GestureDetector(this, gestureListener)
-
-        binding.flingView.setOnTouchListener { v, event ->
+        spring_view.setOnTouchListener { v, event ->
             v.performClick()
-            gestureDetector.onTouchEvent(event)
+            //Проверяем какое действие у нас произошло
+            when(event.action) {
+                //MotionEvent.ACTION_DOWN - вызывается, когда ваш палец коснулся экрана, то есть как бы опустился
+                //вниз, поэтому и DOWN
+                MotionEvent.ACTION_DOWN -> {
+                    //Устанавливаем координаты для корректного перемещения
+                    diffX = event.rawX - v.x
+                    diffY = event.rawY - v.y
+
+                    //Отменяем анимацию, если к примера нашу view еще "пружинит" с предыдущего раза
+                    springAnimationX.cancel()
+                    springAnimationY.cancel()
+                }
+                //MotionEvent.ACTION_MOVE - вызывается, когда мы перемещаем view, то есть меняются координаты
+                //view
+                MotionEvent.ACTION_MOVE -> {
+                    //rawX, rawY текущие координаты view
+                    spring_view.x = event.rawX - diffX
+                    spring_view.y = event.rawY - diffY
+                }
+                //MotionEvent.ACTION_UP - вызывается, когда палец перестал касаться экрана
+                MotionEvent.ACTION_UP -> {
+                    //Стартуем анимацию возвращения в прежнее положение
+                    springAnimationX.start()
+                    springAnimationY.start()
+                }
+            }
+            true
         }
+
     }
 }
 
