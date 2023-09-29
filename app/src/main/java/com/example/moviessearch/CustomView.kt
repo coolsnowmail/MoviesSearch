dfb394306b4ceef2fb799bcb66d948a20cce7573
And <cool.snow@mail.ru>
2023-09-29 15:45:15 +0300

    30 unit

--- app/src/main/java/com/example/moviessearch/CustomView.kt
+++ app/src/main/java/com/example/moviessearch/CustomView.kt
@@ -1,13 +1,12 @@
 package com.example.moviessearch
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
 import android.util.AttributeSet
 import android.util.Log
 import android.view.View
 import com.megamovies.moviessearch.R
+
 
 class CustomView @JvmOverloads constructor(context: Context, attributeSet: AttributeSet? = null) :
     View(context, attributeSet) {
@@ -25,13 +24,24 @@ class CustomView @JvmOverloads constructor(context: Context, attributeSet: Attri
         }
     }
 
+    private val paint = Paint().apply {
+        color = Color.BLACK
+        style = Paint.Style.FILL_AND_STROKE
+        strokeWidth = strokeWidthAttr
+        flags = Paint.ANTI_ALIAS_FLAG
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        paint.shader =
+            LinearGradient(0f, 0f, 0f, w.toFloat(), Color.BLUE, Color.YELLOW, Shader.TileMode.MIRROR)
+    }
 
     override fun onDraw(canvas: Canvas?) {
-        val paint = Paint().apply {
-            color = Color.GREEN
-            style = Paint.Style.STROKE
-            strokeWidth = strokeWidthAttr
-        }
-        canvas?.drawLine(0f, 0f, width.toFloat(), height.toFloat(), paint)
+        canvas?.drawCircle(width / 2F, height / 2F, 400F, paint)
     }
+
+//    override fun onDraw(canvas: Canvas?) {
+//        canvas?.drawPoints(floatArrayOf(0F,0F,0F,300F,300F,0F),paint)
+//    }
+
 }
