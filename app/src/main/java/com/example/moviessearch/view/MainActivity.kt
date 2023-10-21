0e92284a86ca2740ea45bcb1ebbd450cef6ee1e5
Анастасия <cool.sbow@mail.ru>
2023-10-21 22:48:19 +0300

    dependency set for 35

--- app/src/main/java/com/example/moviessearch/view/MainActivity.kt
+++ app/src/main/java/com/example/moviessearch/view/MainActivity.kt
@@ -5,10 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.RequestBuilder
 import com.example.moviessearch.*
 import com.example.moviessearch.view.fragments.*
+import com.google.gson.Gson
 import com.megamovies.moviessearch.R
 import com.megamovies.moviessearch.databinding.ActivityMainBinding
+import okhttp3.*
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.net.URL
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+import javax.net.ssl.HttpsURLConnection
 
 
 class MainActivity : AppCompatActivity() {
@@ -24,6 +34,44 @@ class MainActivity : AppCompatActivity() {
             .addToBackStack(
                 null
             ).commit()
+
+
+        //        task 34.5.1
+        Executors.newSingleThreadExecutor().execute {
+            val url = URL("https://reqres.in/api/users/2")
+            val connection = url.openConnection() as HttpsURLConnection
+            try {
+                val bufferReader = BufferedReader(InputStreamReader(connection.inputStream))
+                val line = bufferReader.readLine()
+                println("!!!! HttpsURLConnection $line")
+            } finally {
+                connection.disconnect()
+            }
+        }
+
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url("https://reqres.in/api/users/2")
+            .build()
+        client.newCall(request).enqueue(object: Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                try {
+                    val responseBody = response.body()
+                    println("!!!! OkHttpClient ${responseBody?.string()}")
+                } catch (e:Exception) {
+                    println(response)
+                    e.printStackTrace()
+                }
+            }
+
+        })
+
+        val gson = Gson()
+        //        task 34.5.1
     }
 
     var backPressed = 0L
@@ -59,7 +107,8 @@ class MainActivity : AppCompatActivity() {
             .addToBackStack(null).commit()
     }
 
-    private fun checkFragmentExistence(tag: String): Fragment? = supportFragmentManager.findFragmentByTag(tag)
+    private fun checkFragmentExistence(tag: String): Fragment? =
+        supportFragmentManager.findFragmentByTag(tag)
 
     private fun changeFragment(fragment: Fragment, tag: String) {
         supportFragmentManager
@@ -68,6 +117,7 @@ class MainActivity : AppCompatActivity() {
             .addToBackStack(null)
             .commit()
     }
+
     private fun initNavigation() {
 
         binding.bottomNavigation.setOnNavigationItemSelectedListener {
@@ -76,28 +126,28 @@ class MainActivity : AppCompatActivity() {
                 R.id.home -> {
                     val tag = "home"
                     val fragment = checkFragmentExistence(tag)
-                    changeFragment(fragment?: HomeFragment(), tag)
+                    changeFragment(fragment ?: HomeFragment(), tag)
                     true
                 }
 
                 R.id.favorites -> {
                     val tag = "favorites"
                     val fragment = checkFragmentExistence(tag)
-                    changeFragment(fragment?: FavoritesFragment(), tag)
+                    changeFragment(fragment ?: FavoritesFragment(), tag)
                     true
                 }
 
                 R.id.watch_later -> {
                     val tag = "watch_later"
                     val fragment = checkFragmentExistence(tag)
-                    changeFragment(fragment?: SeeLaterFragment(), tag)
+                    changeFragment(fragment ?: SeeLaterFragment(), tag)
                     true
                 }
 
                 R.id.selections -> {
                     val tag = "selections"
                     val fragment = checkFragmentExistence(tag)
-                    changeFragment(fragment?: CollectionsFragment(), tag)
+                    changeFragment(fragment ?: CollectionsFragment(), tag)
                     true
                 }
 
