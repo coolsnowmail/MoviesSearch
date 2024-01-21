77bb4646af6af714903901146671de0fdf975093
San Andres <cool.snow@mail.ru>
2024-01-21 22:10:26 +0300

    databasemodule

--- app/src/main/java/com/example/moviessearch/di/modules/DatabaseModule.kt
+++ app/src/main/java/com/example/moviessearch/di/modules/DatabaseModule.kt
@@ -1,4 +1,13 @@
 package com.example.moviessearch.di.modules
 
+import com.example.moviessearch.data.MainRepository
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
 class DatabaseModule {
+    @Provides
+    @Singleton
+    fun provideRepository() = MainRepository()
 }
